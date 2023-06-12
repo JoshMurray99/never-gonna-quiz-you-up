5b486c4244ec4a7ff891fdf4b4c5411f8d70058c
Oliver Thomas <oliver.thomas20011@gmail.com>
2023-06-12 11:28:20 +0100

    Added get request for subject leaderboards

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+
 const histQuestions = require("./questionsList/historyQuestions.json");
 const geoQuestions = require("./questionsList/geographyQuestions.json");
+const histLeaderboard = require("./Leaderboards/historyLeaderboard.json");
+const geoLeaderboard = require("./Leaderboards/geographyLeaderboard.json");
 
 const numQuestions = 15;
 
@@ -14,13 +17,30 @@ app.get('/', (req,res) => {
 })
 
 app.get('/:subject', (req, res) => {
-    let subject = req.params.subject.toLowerCase();
+    const subject = req.params.subject.toLowerCase();
     if (subject === 'history') {
         res.send(shuffleArr(histQuestions).slice(0, numQuestions));
     } else if (subject === 'geography') {
         res.send(shuffleArr(geoQuestions).slice(0, numQuestions));
+    } else {
+        res.sendStatus(404);
     }
 })
+
+app.get('/:subject/leaderboard', (req, res) => {
+    const subject = req.params.subject.toLowerCase();
+    if (subject === 'history') {
+        res.send(histLeaderboard.sort((obj1, obj2) => (obj1.score > obj2.score) ? -1 : (obj1.score < obj2.score) ? 1 : 0));
+    } else if (subject === 'geography') {
+        res.send(geoLeaderboard.sort((obj1, obj2) => (obj1.score > obj2.score) ? -1 : (obj1.score < obj2.score) ? 1 : 0));
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+
+
+
 
 
 
