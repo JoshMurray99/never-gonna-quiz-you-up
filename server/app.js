0c7d62caf7e29427dd4873e871da2533a902b3e3
Oliver Thomas <oliver.thomas20011@gmail.com>
2023-06-12 10:56:49 +0100

    Refactored subject get requests to send 15 random questions from their respective json file

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express();
 const histQuestions = require("./questionsList/historyQuestions.json");
 const geoQuestions = require("./questionsList/geographyQuestions.json");
 
+const numQuestions = 15;
 
 app.use(cors());
 app.use(express.json());
@@ -12,12 +13,27 @@ app.get('/', (req,res) => {
     res.send('It is working');
 })
 
-app.get('/history', (req, res) => {
-    res.send(histQuestions);
+app.get('/:subject', (req, res) => {
+    let subject = req.params.subject.toLowerCase();
+    if (subject === 'history') {
+        res.send(shuffleArr(histQuestions).slice(0, numQuestions));
+    } else if (subject === 'geography') {
+        res.send(shuffleArr(geoQuestions).slice(0, numQuestions));
+    }
 })
 
-app.get('/geography', (req, res) => {
-    res.send(geoQuestions);
-})
 
+
+
+
+
+function shuffleArr(arr) {
+    const newArr = [];
+    for (let i = arr.length; i>0; i--){
+        let randNum = Math.floor(Math.random()*i)
+        newArr.push(arr[randNum]);
+        arr = arr.slice(0, randNum).concat(arr.slice(randNum + 1, i));
+    }
+    return newArr
+}
 module.exports = app;
