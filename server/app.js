5f51d213fab0c022b72a5d64f326895207066a01
Oliver Thomas <oliver.thomas20011@gmail.com>
2023-06-12 10:07:11 +0100

    Added json files for leaderboards & questions, added get requests for all geo & hist questions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const histQuestions = require("./questionsList/historyQuestions.json");
+const geoQuestions = require("./questionsList/geographyQuestions.json");
 
 
 app.use(cors());
@@ -8,6 +10,14 @@ app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send('It is working');
+})
+
+app.get('/history', (req, res) => {
+    res.send(histQuestions);
+})
+
+app.get('/geography', (req, res) => {
+    res.send(geoQuestions);
 })
 
 module.exports = app;
