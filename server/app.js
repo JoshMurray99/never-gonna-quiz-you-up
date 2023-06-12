26bd92d4de37af1cb0212bece277d5d17e858f1f
Sabrina Wright <sabrinawright18@gmail.com>
2023-06-12 15:18:12 +0100

    Questions added to json file for geography

---
+++ server/app.js
@@ -0,0 +1,83 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const histQuestions = require("./questionsList/historyQuestions.json");
+const geoQuestions = require("./questionsList/geographyQuestions.json");
+const histLeaderboard = require("./Leaderboards/historyLeaderboard.json");
+const geoLeaderboard = require("./Leaderboards/geographyLeaderboard.json");
+
+const numQuestions = 15;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('It is working');
+})
+
+app.get('/:subject', (req, res) => {
+    const subject = req.params.subject.toLowerCase();
+    if (subject === 'history') {
+        res.send(shuffleArr(histQuestions).slice(0, numQuestions));
+    } else if (subject === 'geography') {
+        res.send(shuffleArr(geoQuestions).slice(0, numQuestions));
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+app.get('/:subject/leaderboard', (req, res) => {
+    const subject = req.params.subject.toLowerCase();
+    if (subject === 'history') {
+        res.send(histLeaderboard.sort((obj1, obj2) => (obj1.score > obj2.score) ? -1 : (obj1.score < obj2.score) ? 1 : 0));
+    } else if (subject === 'geography') {
+        res.send(geoLeaderboard.sort((obj1, obj2) => (obj1.score > obj2.score) ? -1 : (obj1.score < obj2.score) ? 1 : 0));
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+app.post('/:subject', (req, res) => {
+    const name = req.body.name.toLowerCase();
+    const score = Number(req.body.score);
+    const subject = req.params.subject.toLowerCase();
+
+    if (subject === "history") {
+        histLeaderboard.push({"name":name, "score":score})
+        const data = JSON.stringify(histLeaderboard);
+        fs.writeFile("./server/Leaderboards/historyLeaderboard.json", data, writeFileError);
+        res.sendStatus(201);
+    } else if (subject === "geography") {
+        geoLeaderboard.push({"name":name, "score":score})
+        const data = JSON.stringify(geoLeaderboard);
+        fs.writeFile("./server/Leaderboards/geographyLeaderboard.json", data, writeFileError);
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(409);
+    }
+})
+
+
+
+
+
+
+function writeFileError(error) {
+    if (error) {
+        res.sendStatus(400);
+        console.log(error);
+    }
+}
+
+function shuffleArr(arr) {
+    const newArr = [];
+    for (let i = arr.length; i>0; i--){
+        let randNum = Math.floor(Math.random()*i)
+        newArr.push(arr[randNum]);
+        arr = arr.slice(0, randNum).concat(arr.slice(randNum + 1, i));
+    }
+    return newArr
+}
+module.exports = app;
